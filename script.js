90c0755a597b4319f24a4946a5058ba56af0e471
Michael <sxs.linux@hotmail.com>
2020-03-24 19:22:30 -0400

    Updated calculator exercises

--- script.js
+++ script.js
@@ -32,22 +32,35 @@ console.log(printReady);
 // Calculator
 // Write a function called add. It should accept two numbers as parameters and log their sum to the console.
 function add(numberOne, numberTwo){
-    console.log(numberOne + numberTwo);
+    var sum = numberOne + numberTwo;
+    return sum;
 }
-add(1, 1);
+newSum = add(1, 1);
+console.log(newSum);
 // Write a function called subtract. It should accept two numbers as parameters and log the difference between the first and second number to the console.
 function subtract(numberOne, numberTwo){
-    console.log(numberOne - numberTwo);
+    var sum = numberOne - numberTwo;
+    return sum;
 }
-subtract(2, 1);
+anotherSum = subtract(2, 1);
+console.log(anotherSum);
 // Write a function called multiply. It should accept two numbers as parameters and log their product to the console.
 function multiply(numberOne, numberTwo){
-    console.log(numberOne * numberTwo);
+    var sum = numberOne * numberTwo;
+    return sum;
 }
-multiply(5, 2);
+var yetAnotherSum = multiply(5, 2);
+console.log(yetAnotherSum);
 // Write a function called divide. It should accept two numbers as parameters, divide the first number by the second number, and log the result to the console.
 function divide(numberOne, numberTwo) {
-    console.log(numberOne / numberTwo);
+    var sum = numberOne / numberTwo;
+    return sum;
 }
-divide(10, 2);
+var divSum = divide(10, 2);
+console.log(divSum);
 // Call each function three times with different parameters.
+
+
+// Calculator Refactor
+// Refactor your previous exercise so that each calculator function returns the result of its calculations. You should not have any console.logs inside your functions.
+// When you execute your calculator functions, store each result in a new variable and log the variable to the console. The console.logs should now be outside the scope of your functions.
